2e1a28b24eff0d31e25ae8b408a959bd8c3c3c36
YonatanGale <yonatangale2@gmail.com>
2022-08-06 18:03:58 -0400

    views pruebas

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def myfirstview(request):
+    data = {
+            'name': "Yoni"
+    } 
+    return JsonResponse(data)
