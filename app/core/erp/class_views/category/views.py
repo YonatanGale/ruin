c530fbd837c1892d0e35470f5e378d794d1cbfa9
YonatanGale <yonatangale2@gmail.com>
2022-08-14 03:38:32 -0400

    pruebas

--- app/core/erp/class_views/category/views.py
+++ app/core/erp/class_views/category/views.py
@@ -1,12 +1,19 @@
 from unicodedata import category
+from urllib import request
 from django.shortcuts import render
 from core.erp.models import Category
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 
 
 class categoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
+
+    # @method_decorator(login_required)
+    # def dispatch(self, request, *args, **kwargs):
+    #     return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
