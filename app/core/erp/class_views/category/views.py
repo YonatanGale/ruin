179a17d3a1f458e9adf9317f0fe31b08083a887b
YonatanGale <yonatangale2@gmail.com>
2022-08-14 00:54:58 -0400

    datatables

--- app/core/erp/class_views/category/views.py
+++ app/core/erp/class_views/category/views.py
@@ -3,15 +3,12 @@ from django.shortcuts import render
 from core.erp.models import Category
 from django.views.generic import ListView
 
-def category_list(request):
-    data = {
-        'title': 'Listado de categorias',
-        'categories': Category.objects.all()
-    }
-    return render(request, 'category/list.html', data)
 
 class categoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
 
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Lista de categorias'
+        return context
--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from core.erp.class_views.category.views import category_list
+from core.erp.class_views.category.views import *
 
 app_name = 'erp'
 
 urlpatterns = [
-    path('category/list/', category_list, name='category_list')
+    path('category/list/', categoryListView.as_view(), name='category_list')
 ]
