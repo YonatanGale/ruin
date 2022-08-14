747e9d6cb357ad2c072ed349f4b352f754e59c5e
YonatanGale <yonatangale2@gmail.com>
2022-08-13 23:53:09 -0400

    list

--- app/core/erp/class_views/category/views.py
+++ app/core/erp/class_views/category/views.py
@@ -1,7 +1,17 @@
+from unicodedata import category
 from django.shortcuts import render
+from core.erp.models import Category
+from django.views.generic import ListView
 
 def category_list(request):
     data = {
-        'title': 'Listado de categorias'
+        'title': 'Listado de categorias',
+        'categories': Category.objects.all()
     }
     return render(request, 'category/list.html', data)
+
+class categoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+
